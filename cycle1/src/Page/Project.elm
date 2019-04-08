a3ab0790f43d4d54a27e0ecb6ea0f303babcaf3e
Julien Donck <donckjulien@gmail.com>
2019-04-09 00:28:48 +0200

    added people section in project

--- cycle1/src/Page/Project.elm
+++ cycle1/src/Page/Project.elm
@@ -3,6 +3,7 @@ module Page.Project exposing (view)
 import Element exposing (Element)
 import Page.Project.Actions as Actions
 import Page.Project.Header as Header
+import Page.Project.People as People
 
 
 
@@ -12,5 +13,5 @@ import Page.Project.Header as Header
 view : ( String, Element msg )
 view =
     ( "project"
-    , Element.column [ Element.width (Element.px 1074) ] [ Header.view, Actions.view ]
+    , Element.column [ Element.width (Element.px 1074) ] [ Header.view, Actions.view, People.view ]
     )
