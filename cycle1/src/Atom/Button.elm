f38d83e97a9559353fe57155638f00dac9c705f9
Julien Donck <donckjulien@gmail.com>
2019-05-03 11:48:31 +0200

    Updates button hover state (#26)
    * updates button hover state
    
    * replaces custom button with transparent button

--- cycle1/src/Atom/Button.elm
+++ cycle1/src/Atom/Button.elm
@@ -21,32 +21,32 @@ import Style.Font as Font
 
 primary : List (Attribute msg) -> String -> Element msg
 primary attrs btnText =
-    style attrs Color.purple Color.white Color.black btnText
+    style attrs Color.purple Color.white (Color.alpha Color.purple 0.85) btnText
 
 
 secondary : List (Attribute msg) -> String -> Element msg
 secondary attrs btnText =
-    style attrs Color.lightGrey Color.darkGrey Color.black btnText
+    style attrs Color.lightGrey Color.darkGrey (Color.alpha Color.lightGrey 0.85) btnText
 
 
 accent : List (Attribute msg) -> String -> Element msg
 accent attrs btnText =
-    style attrs Color.pink Color.white Color.black btnText
+    style attrs Color.pink Color.white (Color.alpha Color.pink 0.85) btnText
 
 
 secondaryAccent : List (Attribute msg) -> String -> Element msg
 secondaryAccent attrs btnText =
-    style attrs Color.green Color.white Color.black btnText
+    style attrs Color.green Color.white (Color.alpha Color.green 0.85) btnText
 
 
 custom : List (Attribute msg) -> Element.Color -> Element.Color -> String -> Element msg
 custom attrs color textColor btnText =
-    style attrs color textColor Color.black btnText
+    style attrs color textColor (Color.alpha color 0.85) btnText
 
 
 transparent : List (Attribute msg) -> String -> Element msg
 transparent attrs btnText =
-    style attrs (Color.alpha Color.white 0) Color.darkGrey (Color.alpha Color.white 0) btnText
+    style attrs (Color.alpha Color.white 0) Color.darkGrey (Color.alpha Color.almostWhite 0.5) btnText
 
 
 style : List (Attribute msg) -> Element.Color -> Element.Color -> Element.Color -> String -> Element msg
