1c42ab7eab97f478b92009bc80a48ea1f02669e3
Julien Donck <donckjulien@gmail.com>
2019-05-03 15:08:15 +0200

    Styling of the first step of the register flow

--- cycle1/src/Page/Register.elm
+++ cycle1/src/Page/Register.elm
@@ -2,10 +2,12 @@ module Page.Register exposing (Model, Msg(..), init, update, view)
 
 import Atom.Button as Button
 import Atom.Heading as Heading
+import Atom.Icon as Icon
 import Element exposing (Attribute, Element)
+import Element.Background as Background
 import Element.Border as Border
 import Element.Events as Events
-import Element.Font as Font
+import Element.Font
 import Element.Input as Input
 import Page.Register.Contract as Contract
 import Project exposing (Project)
@@ -151,11 +153,13 @@ update msg (Model oldStep oldProject fieldError) =
 viewInfoFormError : Bool -> Element Msg
 viewInfoFormError show =
     if show then
-        Element.el
-            [ Font.color Color.red
+        Element.row
+            ([ Element.paddingEach { top = 0, left = 366, bottom = 0, right = 0 } ]
+                ++ Font.mediumBodyText Color.red
+            )
+            [ Icon.alert
+            , Element.el [ Element.paddingXY 8 0 ] <| Element.text "Not all mandatory fields have been filled in."
             ]
-        <|
-            Element.text "Not all mandatory fields have been filled in."
 
     else
         Element.none
@@ -165,13 +169,14 @@ viewInfoNext : Bool -> Element Msg
 viewInfoNext blocked =
     if blocked then
         Element.el
-            []
+            [ Element.alignRight ]
         <|
-            Button.secondary [] "Next"
+            Button.inactive [] "Next"
 
     else
         Element.el
             [ Events.onClick <| MoveStepContract
+            , Element.alignRight
             ]
         <|
             Button.primary [] "Next"
@@ -184,15 +189,18 @@ viewInfo project (FieldError nameError codeHostError) =
             Project.meta project
     in
     Element.column
-        []
-        [ Heading.section
+        [ Element.width Element.fill
+        , Element.spacing 32
+        ]
+        [ Heading.sectionWithDesc
             [ Border.color Color.lightGrey
             , Border.widthEach { top = 0, right = 0, bottom = 1, left = 0 }
             ]
             "Project information"
+            "This is what users will see when they land on your project page. ( *Mandatory fields )"
         , Input.text
-            ([ Events.onLoseFocus BlurName
-             ]
+            ([ Events.onLoseFocus BlurName ]
+                ++ inputAttrs
                 ++ (if nameError then
                         errorAttrs
 
@@ -200,7 +208,7 @@ viewInfo project (FieldError nameError codeHostError) =
                         []
                    )
             )
-            { label = Input.labelLeft [] <| Element.text "name*"
+            { label = viewInput "Name*"
             , onChange = UpdateName
             , placeholder = Nothing
             , text = Meta.name meta
@@ -208,6 +216,7 @@ viewInfo project (FieldError nameError codeHostError) =
         , Input.text
             ([ Events.onLoseFocus BlurCodeHost
              ]
+                ++ inputAttrs
                 ++ (if codeHostError then
                         errorAttrs
 
@@ -215,35 +224,53 @@ viewInfo project (FieldError nameError codeHostError) =
                         []
                    )
             )
-            { label = Input.labelLeft [] <| Element.text "code hosting url*"
+            { label = viewInput "code hosting url*"
             , onChange = UpdateCodeHostUrl
             , placeholder = Nothing
             , text = Meta.codeHostUrl meta
             }
         , Input.text
-            []
-            { label = Input.labelLeft [] <| Element.text "description"
+            inputAttrs
+            { label = viewInput "description"
             , onChange = UpdateDescription
-            , placeholder = Just <| Input.placeholder [] <| Element.text "max 200 characters"
+            , placeholder = Just <| Input.placeholder [ Element.paddingXY 12 9 ] <| Element.text "max 200 characters"
             , text = Meta.description meta
             }
         , Input.text
-            []
-            { label = Input.labelLeft [] <| Element.text "website url"
+            inputAttrs
+            { label = viewInput "website url"
             , onChange = UpdateWebsiteUrl
             , placeholder = Nothing
             , text = Meta.websiteUrl meta
             }
         , Input.text
-            []
-            { label = Input.labelLeft [] <| Element.text "image url"
+            inputAttrs
+            { label = viewInput "image url"
             , onChange = UpdateImageUrl
-            , placeholder = Just <| Input.placeholder [] <| Element.text "svg, png or jpg - max 400 x 400 px"
+            , placeholder = Just <| Input.placeholder [ Element.paddingXY 12 9 ] <| Element.text "svg, png or jpg - max 400 x 400 px"
             , text = Meta.imageUrl meta
             }
-        , viewInfoFormError (nameError || codeHostError)
-        , viewInfoNext (nameError || codeHostError || Meta.name meta == "" || Meta.codeHostUrl meta == "")
+        , Element.row
+            [ Element.width Element.fill
+            , Element.paddingEach { top = 0, left = 0, bottom = 96, right = 0 }
+            ]
+            [ viewInfoFormError (nameError || codeHostError)
+            , viewInfoNext (nameError || codeHostError || Meta.name meta == "" || Meta.codeHostUrl meta == "")
+            ]
         ]
+
+
+viewInput : String -> Input.Label msg
+viewInput label =
+    Input.labelLeft [ Element.width <| Element.px 366 ] <|
+        Element.el
+            ([ Element.alignRight
+             , Element.paddingXY 24 9
+             ]
+                ++ Font.mediumBodyText Color.darkGrey
+            )
+        <|
+            Element.text label
 
 
 viewPreview : Project -> Element Msg
@@ -285,7 +312,7 @@ view (Model step project fieldError) =
     in
     ( "register"
     , Element.column
-        [ Element.width (Element.px 1074) ]
+        [ Element.width (Element.px 1074), Element.centerX ]
         [ Element.el
             ([ Element.centerX
              ]
@@ -293,9 +320,36 @@ view (Model step project fieldError) =
             )
           <|
             Element.text "Register your project"
+        , viewProgress step
         , viewStep
         ]
     )
+
+
+viewProgress : Step -> Element msg
+viewProgress step =
+    let
+        ( pos, ( infoColor, contractColor, previewColor ) ) =
+            case step of
+                Info ->
+                    ( 0, ( Color.purple, Color.grey, Color.grey ) )
+
+                Contract ->
+                    ( 1, ( Color.grey, Color.purple, Color.grey ) )
+
+                Preview ->
+                    ( 2, ( Color.grey, Color.grey, Color.purple ) )
+    in
+    Element.column
+        [ Element.centerX, Element.paddingXY 0 64 ]
+        [ Element.row
+            [ Element.width <| Element.px 370, Element.paddingXY 0 12 ]
+            [ Element.el ([ Element.width <| Element.px 88, Element.Font.center ] ++ Font.mediumBodyText infoColor) <| Element.text "Information"
+            , Element.el ([ Element.width <| Element.px 88, Element.Font.center, Element.centerX ] ++ Font.mediumBodyText contractColor) <| Element.text "Contract"
+            , Element.el ([ Element.width <| Element.px 88, Element.Font.center, Element.alignRight ] ++ Font.mediumBodyText previewColor) <| Element.text "Preview"
+            ]
+        , Element.el [ Element.centerX ] <| Icon.progress pos
+        ]
 
 
 
@@ -307,3 +361,13 @@ errorAttrs =
     [ Border.color Color.red
     , Border.width 1
     ]
+
+
+inputAttrs : List (Attribute msg)
+inputAttrs =
+    [ Element.height <| Element.px 36
+    , Background.color Color.almostWhite
+    , Border.color Color.lightGrey
+    , Border.width 1
+    ]
+        ++ Font.mediumBodyText Color.black
