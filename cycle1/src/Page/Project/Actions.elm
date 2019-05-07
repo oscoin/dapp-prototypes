4ca27efa253ee428cec3601b2042b02455910e28
Alexander Simmerl <a.simmerl@gmail.com>
2019-05-07 17:47:40 +0200

    Display overlay

--- cycle1/src/Page/Project/Actions.elm
+++ cycle1/src/Page/Project/Actions.elm
@@ -1,7 +1,9 @@
 module Page.Project.Actions exposing (view)
 
 import Atom.Button as Button
-import Element exposing (Element)
+import Element exposing (Attribute, Element)
+import Element.Background as Background
+import Style.Color as Color
 
 
 
@@ -10,22 +12,53 @@ import Element exposing (Element)
 
 view : Bool -> Element msg
 view isMaintainer =
-    let
-        actions =
-            if isMaintainer then
-                [ Button.primary [] "Checkpoint your project"
-                , Button.secondaryAccent [] "Share your project"
-                ]
+    if isMaintainer then
+        viewMaintainer
 
-            else
-                [ Button.primary [] "Donate"
-                , Button.secondary [] "Follow"
-                , Button.secondaryAccent [] "Support"
-                ]
-    in
+    else
+        viewViewer
+
+
+viewMaintainer : Element msg
+viewMaintainer =
     Element.row
-        [ Element.spacing 16
-        , Element.paddingEach { top = 16, right = 0, bottom = 0, left = 0 }
-        , Element.width Element.fill
+        (listAttrs
+            ++ [ Element.below viewCheckpointOverlay
+               ]
+        )
+        [ Button.primary [] "Checkpoint your project"
+        , Button.secondaryAccent [] "Share your project"
         ]
-        actions
+
+
+viewCheckpointOverlay : Element msg
+viewCheckpointOverlay =
+    Element.el
+        [ Background.color Color.white
+        , Element.height <| Element.px 100
+        , Element.width <| Element.px 100
+        ]
+    <|
+        Element.text "COPY THIS"
+
+
+viewViewer : Element msg
+viewViewer =
+    Element.row
+        listAttrs
+        [ Button.primary [] "Donate"
+        , Button.secondary [] "Follow"
+        , Button.secondaryAccent [] "Support"
+        ]
+
+
+
+-- ATTRS
+
+
+listAttrs : List (Attribute msg)
+listAttrs =
+    [ Element.paddingEach { top = 16, right = 0, bottom = 0, left = 0 }
+    , Element.spacing 16
+    , Element.width Element.fill
+    ]
