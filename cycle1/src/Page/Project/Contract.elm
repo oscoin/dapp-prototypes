7d3576ed85c041848aa50a83a6ff6a751eddb8f0
Julien Donck <donckjulien@gmail.com>
2019-05-07 16:28:41 +0200

    Adds empty states and maintainer states

--- cycle1/src/Page/Project/Contract.elm
+++ cycle1/src/Page/Project/Contract.elm
@@ -1,5 +1,6 @@
 module Page.Project.Contract exposing (view)
 
+import Atom.Button as Button
 import Atom.Heading as Heading
 import Element exposing (Element)
 import Element.Border as Border
@@ -12,18 +13,31 @@ import Style.Color as Color
 -- VIEW
 
 
-view : Contract -> Element msg
-view contract =
+view : Contract -> Bool -> Element msg
+view contract isMaintainer =
+    let
+        header =
+            if isMaintainer then
+                Heading.sectionWithInfo
+                    [ Border.color Color.lightGrey
+                    , Border.widthEach { top = 0, right = 0, bottom = 1, left = 0 }
+                    ]
+                    "Project contract"
+                    (Button.transparent [] "Edit")
+
+            else
+                Heading.section
+                    [ Border.color Color.lightGrey
+                    , Border.widthEach { top = 0, right = 0, bottom = 1, left = 0 }
+                    ]
+                    "Project contract"
+    in
     Element.column
         [ Element.spacing 24
         , Element.paddingEach { top = 44, right = 0, bottom = 0, left = 0 }
         , Element.width Element.fill
         , Element.centerX
         ]
-        [ Heading.section
-            [ Border.color Color.lightGrey
-            , Border.widthEach { top = 0, right = 0, bottom = 1, left = 0 }
-            ]
-            "Project contract"
+        [ header
         , ContractPreview.view [] contract
         ]
--- cycle1/src/Page/Project/People.elm
+++ cycle1/src/Page/Project/People.elm
@@ -4,6 +4,7 @@ import Atom.Button as Button
 import Atom.Heading as Heading
 import Element exposing (Element)
 import Element.Border as Border
+import Element.Font
 import Person as Person exposing (Person)
 import Style.Color as Color
 import Style.Font as Font
@@ -13,20 +14,111 @@ import Style.Font as Font
 -- VIEW
 
 
-view : List Person -> List Person -> Element msg
-view maintainers contributors =
+view : List Person -> List Person -> Bool -> Element msg
+view maintainers contributors isMaintainer =
     Element.row
         [ Element.spacing 24
         , Element.paddingEach { top = 64, right = 0, bottom = 0, left = 0 }
         , Element.width Element.fill
         ]
-        [ viewTopPeople "Maintainers" <| List.take 4 maintainers
-        , viewTopPeople "Top contributors" <| List.take 4 contributors
+        [ viewMaintainers "Maintainers" maintainers isMaintainer
+        , viewContributors "Contributors" contributors isMaintainer
         ]
 
 
-viewTopPeople : String -> List Person -> Element msg
-viewTopPeople title people =
+viewMaintainers : String -> List Person -> Bool -> Element msg
+viewMaintainers title people isMaintainer =
+    let
+        heading =
+            if isMaintainer then
+                Heading.sectionWithCountInfo [] title (List.length people) (Button.secondary [] "Add")
+
+            else
+                Heading.sectionWithCount [] title (List.length people)
+
+        viewMoreBtn =
+            if List.length people < 5 then
+                Element.none
+
+            else
+                Button.transparent
+                    [ Element.width Element.fill
+                    , Border.color Color.lightGrey
+                    , Border.widthEach { top = 1, right = 0, bottom = 0, left = 0 }
+                    ]
+                    "View all"
+    in
+    Element.column
+        [ Border.color Color.lightGrey
+        , Border.rounded 2
+        , Border.width 1
+        , Element.height (Element.px 255)
+        , Element.width <| Element.fillPortion 1
+        ]
+        -- Dependents
+        [ heading
+        , Element.wrappedRow
+            [ Element.paddingEach { top = 12, right = 24, bottom = 0, left = 24 }
+            , Element.width <| Element.fillPortion 1
+            , Element.height Element.fill
+            ]
+          <|
+            List.map viewTopPeopleSingle <|
+                List.take 4 people
+        , viewMoreBtn
+        ]
+
+
+viewContributors : String -> List Person -> Bool -> Element msg
+viewContributors title people isMaintainer =
+    let
+        viewMoreBtn =
+            if List.length people < 5 then
+                Element.none
+
+            else
+                Button.transparent
+                    [ Element.width Element.fill
+                    , Border.color Color.lightGrey
+                    , Border.widthEach { top = 1, right = 0, bottom = 0, left = 0 }
+                    ]
+                    "View all"
+
+        emptyStateCopy =
+            if isMaintainer then
+                "By checkpointing your project we’ll be able to determine who your contributors are."
+
+            else
+                "No contributors to this project"
+
+        content =
+            if List.length people > 0 then
+                Element.wrappedRow
+                    [ Element.paddingEach { top = 12, right = 24, bottom = 0, left = 24 }
+                    , Element.width <| Element.fillPortion 1
+                    , Element.height Element.fill
+                    ]
+                <|
+                    List.map viewTopPeopleSingle <|
+                        List.take 4 people
+
+            else
+                Element.el
+                    [ Element.width Element.fill
+                    , Element.height Element.fill
+                    ]
+                <|
+                    Element.paragraph
+                        ([ Element.centerX
+                         , Element.centerY
+                         , Element.width
+                            (Element.fill |> Element.maximum 400)
+                         , Element.Font.center
+                         ]
+                            ++ Font.bodyText Color.grey
+                        )
+                        [ Element.text emptyStateCopy ]
+    in
     Element.column
         [ Border.color Color.lightGrey
         , Border.rounded 2
@@ -36,18 +128,8 @@ viewTopPeople title people =
         ]
         -- Dependents
         [ Heading.sectionWithCount [] title (List.length people)
-        , Element.wrappedRow
-            [ Element.paddingEach { top = 12, right = 24, bottom = 0, left = 24 }
-            , Element.width <| Element.fillPortion 1
-            ]
-          <|
-            List.map viewTopPeopleSingle people
-        , Button.transparent
-            [ Element.width Element.fill
-            , Border.color Color.lightGrey
-            , Border.widthEach { top = 1, right = 0, bottom = 0, left = 0 }
-            ]
-            "View all"
+        , content
+        , viewMoreBtn
         ]
 
 
@@ -57,6 +139,7 @@ viewTopPeopleSingle person =
         [ Element.spacing 12
         , Element.width <| Element.fillPortion 1
         , Element.paddingEach { top = 0, right = 0, bottom = 24, left = 0 }
+        , Element.alignTop
         ]
         [ Element.image
             [ Element.height (Element.px 48)
