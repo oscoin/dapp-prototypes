1e6a325a6f4aa775a762f85d87a0cec10d0926bf
Julien Donck <donckjulien@gmail.com>
2019-05-07 10:32:54 +0200

    Adds + styles get started

--- cycle1/src/Page/Project.elm
+++ cycle1/src/Page/Project.elm
@@ -3,6 +3,7 @@ module Page.Project exposing (view)
 import Element exposing (Element)
 import Page.Project.Contract as Contract
 import Page.Project.Fund as Fund
+import Page.Project.GetStarted as GetStarted
 import Page.Project.Graph as Graph
 import Page.Project.Header as Header
 import Page.Project.People as People
@@ -15,6 +16,14 @@ import Project as Project exposing (Project)
 
 view : Project -> ( String, Element msg )
 view project =
+    let
+        viewCheckpointInfo proj =
+            if List.isEmpty <| Project.checkpoints project then
+                Element.el [ Element.paddingEach { top = 44, left = 0, bottom = 0, right = 0 } ] <| GetStarted.view proj
+
+            else
+                Element.none
+    in
     ( "project"
     , Element.column
         [ Element.width Element.fill
@@ -25,7 +34,8 @@ view project =
             [ Element.centerX
             , Element.width <| Element.px 1074
             ]
-            [ Contract.view <| Project.contract project
+            [ viewCheckpointInfo project
+            , Contract.view <| Project.contract project
             , People.view (Project.maintainers project) (Project.contributors project)
             , Fund.view <| Project.funds project
             , Graph.view <| Project.graph project
