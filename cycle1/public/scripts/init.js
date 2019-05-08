730249399cd7c106dbea57d3be599a6b987d760e
Julien Donck <donckjulien@gmail.com>
2019-05-08 14:50:22 +0200

    adds new dependencies to default project

--- cycle1/public/scripts/init.js
+++ cycle1/public/scripts/init.js
@@ -119,6 +119,14 @@ const initialProjects = [
     graph: {
       edges: [
         { direction: 'outgoing', name: 'IPFS', osrank: 0.84 },
+        { direction: 'outgoing', name: 'Cabal', osrank: 0.84 },
+        { direction: 'outgoing', name: 'Other package', osrank: 0.84 },
+        { direction: 'outgoing', name: 'Amazing dependency', osrank: 0.84 },
+        { direction: 'outgoing', name: 'Project wow', osrank: 0.84 },
+        { direction: 'outgoing', name: 'Botcoin', osrank: 0.84 },
+        { direction: 'outgoing', name: 'Bosscoin', osrank: 0.84 },
+        { direction: 'outgoing', name: 'IPFS-front-end', osrank: 0.84 },
+        { direction: 'outgoing', name: 'wowza', osrank: 0.84 },
         { direction: 'incoming', name: 'Julien package', osrank: 0.99 },
       ],
       osrank: 0.86,
