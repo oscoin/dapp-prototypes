37f72df5b468ba8caa2c5ef9c444f0516ccd573b
Julien Donck <donckjulien@gmail.com>
2019-05-10 14:20:02 +0200

    Adds the help text to the "osc checkpoint" command

--- cycle1/cli/osc/bin/index.js
+++ cycle1/cli/osc/bin/index.js
@@ -4,5 +4,5 @@ const program = require('commander')
 
 program
   .command('setup', 'Set up the cli to interact with the network')
-  .command('checkpoint', 'Set up the cli to interact with the network')
+  .command('checkpoint', 'Sends the oscoin network the dependecies and the contributors of this project and allow the network to calculate the osrank of the project')
   .parse(process.argv)
