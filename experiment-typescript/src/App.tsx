c8b79e59763958b090e04e5cb1475889896ffe81
Rūdolfs Ošiņš <rudolfs@osins.org>
2019-08-07 17:21:38 +0200

    Add "New Proposal" button

--- experiment-typescript/src/App.tsx
+++ experiment-typescript/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Button from './elements/Button';
 
 const App: React.FC = () => {
+  const createNewProposal = () => {
+    console.log("bla")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Button onClick={createNewProposal}>New Proposal</Button>
     </div>
   );
 }
